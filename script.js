2d484c47b09e19d26a290652938cee5a307a0009
Nahid <nahid.mansuri@gmail.com>
2020-09-08 20:12:56 +0530

    added d3 lib ref and sample code

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function updateGreeting(){
 
 
 updateGreeting();
+
+var Cirlce = {
+    x: 300,
+    y:300,
+    r:60,
+    stroke : 'blue',
+    width:4,
+    fill: 'yellow'
+}
+
+var svg = d3.select("svg");
+
+svg.select("circle")
+.attr("cx",Cirlce.x)
+.attr("cy",Cirlce.y)
+.attr("r",Cirlce.r)
+.style("stroke",Cirlce.stroke)
+.style("stroke-width",Cirlce.width)
+.style("fill",() =>{
+    if(Cirlce.r < 50) return 'yellow';
+    else return 'green';});
